ebd21f7a258f7f15f268031aa5883ab596115e1d
daniel <ddkozhaev@gmail.com>
2023-09-09 14:27:30 +0300

    Даниил

---
+++ LFQAKnowledgeBase.py
@@ -0,0 +1,95 @@
+import json
+import math
+from typing import List
+
+import datasets
+
+logger = datasets.logging.get_logger(__name__)
+
+_CITATION = ''
+
+_DESCRIPTION = """\
+Выдержки из руководства пользователя для генерации ответов по запросу пользователя.\
+"""
+_LICENSE = ''
+
+_URL = ""
+_URLS = {
+    "train": _URL + "kb.json",
+}
+
+
+class LFQAKBConfig(datasets.BuilderConfig):
+    """билдер конфиг"""
+
+    def __init__(
+            self, language="ru", snippets_length=50, snippets_overlap=25, **kwargs
+    ):
+        """
+        билдер кофинг
+        """
+        super(LFQAKBConfig, self).__init__(**kwargs)
+        self.language = language
+        self.snippets_length = snippets_length
+        self.snippets_overlap = snippets_overlap
+
+
+class LFQASnippets(datasets.GeneratorBasedBuilder):
+    BUILDER_CONFIGS = [LFQAKBConfig(
+        name="LFQAKnowledgeBase",
+        version=datasets.Version("1.0.0"),
+        language="en",
+        snippets_length=50,
+        snippets_overlap=25,
+    )]
+
+    test_dummy_data = False
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "id": datasets.Value("int32"),
+                    "title": datasets.Value("string"),
+                    "heading1": datasets.Value("string"),
+                    "heading2": datasets.Value("string"),
+                    "heading3": datasets.Value("string"),
+                    "heading4": datasets.Value("string"),
+                    "heading5": datasets.Value("string"),
+                    "text": datasets.Value("string"),
+
+                }
+            ),
+            supervised_keys=None,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager: datasets.DownloadManager):
+        downloaded_files = dl_manager.download_and_extract(_URLS)
+
+        return [datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": downloaded_files['train']})]
+
+    def _generate_examples(self, filepath):
+        """This function returns the examples in the raw (text) form."""
+        logger.info("generating examples from = %s", filepath)
+        with open(filepath, encoding='utf-8') as f:
+            lfqa = json.load(f)
+            for article in lfqa:
+                id_ = article.get('id', '')
+                title = article['title']
+                heading1 = article['heading1']
+                heading2 = article['heading2']
+                heading3 = article['heading3']
+                heading4 = article['heading4']
+                heading5 = article['heading5']
+                text = article['text']
+                yield id_, {
+                    "title": title,
+                    "heading1": heading1,
+                    "heading2": heading2,
+                    "heading3": heading3,
+                    "heading4": heading4,
+                    "heading5": heading5,
+                    "text": text
+                }
---
+++ retriever.py
@@ -0,0 +1,40 @@
+from datasets import load_dataset
+from lfqa_utils import *
+
+eli5 = load_dataset('eli5')
+
+kb = load_dataset('kb.json')
+
+print(eli5['test_eli5'][11122])
+
+print(kb['train'][45])
+
+# training arguments
+class ArgumentsQAR():
+    def __init__(self):
+        self.batch_size = 512
+        self.max_length = 128
+        self.checkpoint_batch_size = 32
+        self.print_freq = 100
+        self.pretrained_model_name = "bert-base-multilingual-cased"
+        self.model_save_name = "eli5_retriever_model_l-8_h-768_b-512-512"
+        self.learning_rate = 2e-4
+        self.num_epochs = 10
+
+qar_args = ArgumentsQAR()
+
+# prepare torch Dataset objects
+qar_train_dset = ELI5DatasetQARetriver(eli5['train_eli5'], training=True)
+qar_valid_dset = ELI5DatasetQARetriver(eli5['validation_eli5'], training=False)
+
+# load pre-trained BERT and make model
+qar_tokenizer, qar_model = make_qa_retriever_model(
+        model_name=qar_args.pretrained_model_name,
+        from_file=None,
+        device="cuda:0"
+)
+
+# train the model
+model = train_qa_retriever(qar_model, qar_tokenizer, qar_train_dset, qar_valid_dset, qar_aonrgs)
+
+
