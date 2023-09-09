405f85f31fbfda36bb8f29da4c56db9542ef8b82
Aleksey Krylov <gidramarry@gmail.com>
2023-09-09 22:03:57 +0300

    rework SST & add awaiting_question logic for STT, update reqs

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,6 +1,11 @@
 import pyaudio
 import vosk
 import json
+from text_to_speech import greetings, answering_question
+import time
+
+WAITING_FOR_ACTIVATION = True
+BOT_NAME = 'бот'
 
 model_path = "models_stt/vosk-model-small-ru-0.22" # vosk-model-small-ru-0.22 или vosk-model-ru-0.42
 model = vosk.Model(model_path)
@@ -10,13 +15,25 @@ p = pyaudio.PyAudio() # начинаем слушать микрофон
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
 stream.start_stream()
 
-print("Слушаю...")
-
+print("Ожидание обращения или нажатия на кнопку")
+last_time_heard = 0
 while True:
     data = stream.read(4000, exception_on_overflow=False)
     if rec.AcceptWaveform(data):
         result = json.loads(rec.Result())
         text = result.get('text')
         print(text)
-        if "привет" in text:
-            print("Да, я здесь!")
+
+        if WAITING_FOR_ACTIVATION == True and BOT_NAME in text:
+            greetings()
+            WAITING_FOR_ACTIVATION = False
+            last_time_heard = time.time()
+
+        elif WAITING_FOR_ACTIVATION == False and text != '':
+            #answering_question(text)
+            print('типа ответил на вопрос')
+            last_time_heard = time.time()
+
+        if WAITING_FOR_ACTIVATION == False and time.time() - last_time_heard > 5:
+            WAITING_FOR_ACTIVATION = True
+            print('переход в режим ожидания')
--- text_to_speech.py
+++ text_to_speech.py
@@ -1,34 +1,15 @@
-import os
-import torch
-import sounddevice as sd
+import pyttsx3
+#from model import using_trained_model...
 
+def greetings():
+    engine = pyttsx3.init()     # инициализация
+    engine.setProperty('rate', 150)     # скорость речи
+    engine.setProperty('volume', 0.9)
+    engine.say('   ' + "Приветствую уважаемый, слушаю ваш вопрос!")  # запись фразы в очередь
+    engine.runAndWait()
 
-def text_to_speech():
-    device = torch.device('cpu')
-    torch.set_num_threads(4)
-    local_file = 'models/model.pt'
-
-    if not os.path.isfile(local_file):
-        torch.hub.download_url_to_file('https://models.silero.ai/models/tts/ru/v4_ru.pt',
-                                       local_file)
-
-    model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
-    model.to(device)
-
-    example_text = 'Даниэль приветствую! Я результат синтеза... Надеюсь у тебя получится развернуть сервер с гитлабом.'
-    sample_rate = 48000
-    speaker='aidar' # aidar, baya, kseniya, xenia, eugene, random
-    # audio_paths = model.save_wav(text=example_text,
-    #                              speaker=speaker,
-    #                              sample_rate=sample_rate)
-
-    def speaker_silero(text1):
-        audio = model.apply_tts(text=text1,
-                                    speaker=speaker,
-                                    sample_rate=sample_rate)
-
-        sd.play(audio, blocking=False)
-
-    while True:
-        print('Text please')
-        speaker_silero(input())
+def answering_question(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)
+    engine.setProperty('volume', 0.9)
+    engine.say('   ' + using_trained_model(text))
